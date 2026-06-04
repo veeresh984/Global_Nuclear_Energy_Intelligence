703ace72982919ea5a83c096065ce6068849cd3c
veeresh984 <veereshsbiradar984@gmail.com>
2026-06-04 15:11:28 +0530

    Add decarbonisation intelligence visualization

--- pages/3_Decarbonisation_Intelligence.py
+++ pages/3_Decarbonisation_Intelligence.py
@@ -1,1 +1,26 @@
+import streamlit as st
+import plotly.express as px
 
+from utils.data_loader import load_data
+
+df = load_data()
+
+latest = df[df.year == df.year.max()]
+
+top = (
+    latest
+    .sort_values(
+        "decarbonisation_score",
+        ascending=False
+    )
+    .head(20)
+)
+
+fig = px.bar(
+    top,
+    x="country",
+    y="decarbonisation_score",
+    color="carbon_intensity_tier"
+)
+
+st.plotly_chart(fig, use_container_width=True)
