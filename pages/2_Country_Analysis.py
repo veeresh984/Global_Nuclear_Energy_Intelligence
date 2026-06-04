e67afa7176587e808b7bd081683d0ac5c47116cb
veeresh984 <veereshsbiradar984@gmail.com>
2026-06-04 15:10:25 +0530

    Update 2_Country_Analysis.py

--- pages/2_Country_Analysis.py
+++ pages/2_Country_Analysis.py
@@ -1,1 +1,33 @@
+import streamlit as st
+import plotly.express as px
 
+from utils.data_loader import load_data
+
+df = load_data()
+
+country = st.selectbox(
+    "Select Country",
+    sorted(df.country.unique())
+)
+
+country_df = df[df.country == country]
+
+st.title(country)
+
+fig = px.line(
+    country_df,
+    x="year",
+    y="nuclear_electricity",
+    title="Nuclear Generation Trend"
+)
+
+st.plotly_chart(fig, use_container_width=True)
+
+fig2 = px.line(
+    country_df,
+    x="year",
+    y="renewables_electricity",
+    title="Renewables Trend"
+)
+
+st.plotly_chart(fig2, use_container_width=True)
