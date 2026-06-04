9e6a7c0ad2c4d48ba7abf3ef9e4771fda0eaae5b
veeresh984 <veereshsbiradar984@gmail.com>
2026-06-04 15:11:50 +0530

    Update 5_Carbon_Analytics.py

--- pages/5_Carbon_Analytics.py
+++ pages/5_Carbon_Analytics.py
@@ -1,1 +1,17 @@
+import streamlit as st
+import plotly.express as px
 
+from utils.data_loader import load_data
+
+df = load_data()
+
+latest = df[df.year == df.year.max()]
+
+fig = px.treemap(
+    latest,
+    path=["carbon_intensity_tier","country"],
+    values="co2_mtonne",
+    color="co2_per_capita_t"
+)
+
+st.plotly_chart(fig, use_container_width=True)
